6e62c40957c4ffe4897dfbdce253331523bb0bac
Neville Li <nevilleneville@hotmail.com>
2018-12-15 23:09:32 -0600

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,20 @@
+var socket = io();
+
+socket.on("connect", function() {
+    console.log("connected to server");
+
+
+   socket.emit("createMessage", {
+       from: "Andrew",
+       text: "yup that works for me"
+   });
+
+});
+
+socket.on("disconnect", function() {
+    console.log("Disconnected from server");
+});
+
+socket.on("newMessage", function(message) {
+    console.log("newMessage", message);
+});
--- server/server.js
+++ server/server.js
@@ -1,15 +1,47 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
-const app = express();
+const socketIO = require("socket.io");
 
+const app = express();
 const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
+var server = http.createServer(app);
+var io = socketIO(server);
 
-app.get("/", (req, res) => {
-    res.send("welcome to the chat app");
+
+io.on("connection", (socket) => {
+    console.log("New user connected");
+
+    socket.emit("newEmail", {
+       from: "mike@example.com",
+       text: "hey what is going on",
+       createAt: 123 
+    });
+
+
+    socket.on("createMessage", (message) => {
+        console.log("create Message",message);
+    });
+
+    socket.emit("newMessage", {
+        from: "John",
+        text: "See you then",
+        createdAt: 123123
+    });
+
+
+    socket.on("disconnect", () => {
+        console.log("user was disconnected");
+    });
 });
 
-app.listen(3000, (req, res) => {
+
+
+
+
+
+server.listen(3000, (req, res) => {
     console.log("server started");
 });
